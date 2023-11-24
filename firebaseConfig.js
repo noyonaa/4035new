3d6afe5e9dcd5dab28124785ada641a2a2778e6f
noyonaa <nayanacis@gmail.com>
2023-11-24 14:10:50 +0530

    added more things

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+// firebaseConfig.js
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyACKZfkStXagJ_R9E1DthxDqcSkF4r-ENI",
+    authDomain: "studentcourses-8452f.firebaseapp.com",
+    projectId: "studentcourses-8452f",
+    storageBucket: "studentcourses-8452f.appspot.com",
+    messagingSenderId: "799305562180",
+    appId: "1:799305562180:web:68cd18754f90d6fdc7e411",
+    measurementId: "G-KMNX27ZFSW"
+  };
+  
--- index.js
+++ index.js
@@ -3,20 +3,14 @@ import { getAnalytics } from "firebase/analytics";
 const express = require("express");
 const port = 3000;
 const bodyparser = require("body-parser");
+const firebaseAdmin = require("firebase-admin");
+const path = require('path');
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ entended: false }));
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyACKZfkStXagJ_R9E1DthxDqcSkF4r-ENI",
-  authDomain: "studentcourses-8452f.firebaseapp.com",
-  projectId: "studentcourses-8452f",
-  storageBucket: "studentcourses-8452f.appspot.com",
-  messagingSenderId: "799305562180",
-  appId: "1:799305562180:web:68cd18754f90d6fdc7e411",
-  measurementId: "G-KMNX27ZFSW"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
