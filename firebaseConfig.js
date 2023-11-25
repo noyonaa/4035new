3ff1f1d6c8172a65efb37f84a94ba1833abc5e86
noyonaa <nayanacis@gmail.com>
2023-11-25 18:48:00 +0530

    new changes

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,12 +1,20 @@
 // firebaseConfig.js
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 export const firebaseConfig = {
-    apiKey: "AIzaSyACKZfkStXagJ_R9E1DthxDqcSkF4r-ENI",
-    authDomain: "studentcourses-8452f.firebaseapp.com",
-    projectId: "studentcourses-8452f",
-    storageBucket: "studentcourses-8452f.appspot.com",
-    messagingSenderId: "799305562180",
-    appId: "1:799305562180:web:68cd18754f90d6fdc7e411",
-    measurementId: "G-KMNX27ZFSW"
-  };
+  apiKey: "AIzaSyCzWsivA65DmNsw46US_TOLhDg63fcE4tw",
+  authDomain: "proj-2535e.firebaseapp.com",
+  projectId: "proj-2535e",
+  storageBucket: "proj-2535e.appspot.com",
+  messagingSenderId: "649930705094",
+  appId: "1:649930705094:web:55baba817b64ecaa83efc7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
   
--- index.js
+++ index.js
@@ -5,13 +5,77 @@ const port = 3000;
 const bodyparser = require("body-parser");
 const firebaseAdmin = require("firebase-admin");
 const path = require('path');
+const app = express();
 app.use(bodyparser.json());
+// Serve static files (HTML, CSS, etc.)
+app.use(express.static('public'));
 app.use(bodyparser.urlencoded({ entended: false }));
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Initialize Firebase Admin SDK
+const serviceAccount = require('./serviceAccountKey.json');
+const firebaseConfig = require('./firebaseConfig');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://proj-2535e.firebaseio.com/"
+  // Replace with your Firebase project URL
+});
 
+const firestore = admin.firestore();
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Middleware to check if the user is authenticated
+const isAuthenticated = (req, res, next) => {
+  const idToken = req.header('Authorization');
+  if (!idToken) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  admin.auth().verifyIdToken(idToken)
+    .then((decodedToken) => {
+      req.user = decodedToken;
+      next();
+    })
+    .catch(() => {
+      res.status(401).json({ error: 'Unauthorized' });
+    });
+};
+
+// Serve the home page for authenticated users
+app.get('/', isAuthenticated, (req, res) => {
+  const user = req.user;
+
+  // Retrieve student details from Firestore
+  firestore.collection('students').doc(user.uid).get()
+    .then((doc) => {
+      if (doc.exists) {
+        const studentDetails = doc.data();
+        res.sendFile(path.join(__dirname, 'public', 'home.html'));
+      } else {
+        res.status(404).send('Student details not found.');
+      }
+    })
+    .catch((error) => {
+      console.error('Error retrieving student details:', error);
+      res.status(500).send('Internal Server Error');
+    });
+});
+
+// Serve the registration page
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'registration.html'));
+});
+
+// Serve the login page
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+// Serve the page for selecting courses
+app.get('/select-courses', isAuthenticated, (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'select-courses.html'));
+  // Implement course selection logic here
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
